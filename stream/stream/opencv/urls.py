b30505679ceeee324303e335b23619fca7cfbc07
Denislav <deni_9000_1990@abv.bg>
2022-05-25 09:43:37 +0300

    added basic stream functionality

--- stream/stream/opencv/urls.py
+++ stream/stream/opencv/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from stream.opencv.views import IndexView
+from stream.opencv.views import IndexView, openCam
 
 urlpatterns = [
     path('',IndexView.as_view(),name='index'),
+    path('stream/',openCam,name='cam'),
 ]
--- stream/stream/opencv/views.py
+++ stream/stream/opencv/views.py
@@ -1,6 +1,21 @@
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from  django.views import generic as view_dj
-# Create your views here.
+from stream.opencv.open_cv_helper import VideoCamera, gen
+from django.views.decorators import gzip
+
 
 class IndexView(view_dj.TemplateView):
     template_name = 'index.html'
+
+
+
+
+@gzip.gzip_page
+def openCam(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
+    return render(request, 'stream.html')
